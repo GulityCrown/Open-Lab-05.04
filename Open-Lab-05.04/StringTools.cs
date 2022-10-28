74c4b67ba96142ed9ba0f00d7f350dd83b9f9731
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-28 17:03:35 +0200

    Update StringTools.cs

--- Open-Lab-05.04/StringTools.cs
+++ Open-Lab-05.04/StringTools.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Text;
 
 namespace Open_Lab_05._04
 {
@@ -6,7 +7,20 @@ namespace Open_Lab_05._04
     {
         public string RemoveSpecialCharacters(string original)
         {
-            throw new NotImplementedException();
+            StringBuilder modified = new StringBuilder();
+            modified.Capacity = original.Length;
+            for (int i = 0; i < original.Length; i++)
+            {
+                if ((original[i] >= 65 && original[i] <= 90)
+                    || (original[i] >= 97 && original[i] <= 122
+                        || (original[i] >= 48 && original[i] <= 57
+                            || (original[i] == '-' || original[i] == '_' || original[i] == ' '))))
+                {
+                    modified.Append(original[i]);
+                }
+            }
+
+            return modified.ToString();
         }
     }
 }
